7151a390f630507805e50eb7c8bb6e4524ace937
habibcomsats <habibcomsats@gmail.com>
2021-04-27 11:47:47 +0100

    basic structure

---
+++ trainer/Substra/main.py
@@ -0,0 +1,3 @@
+# This file will be the main entry point of decentralized training network of substra nodes.
+
+# The container will incorporate the substra training modules as well as it will contain the logic to enable decentralized training 
