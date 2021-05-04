8e2cc261e66b63f9beaa3b38c15f97a4c4976a23
habibcomsats <habibcomsats@gmail.com>
2021-05-04 10:51:50 +0100

    coordinator and reporter docker files updated

--- trainer/reporter/src/main.py
+++
@@ -1,3 +0,0 @@
-# this file will be the main entry point of the reporter container
-
-# The container will enable different reporting strategies such as full and particial sharing of model updates and model checkpoints.
