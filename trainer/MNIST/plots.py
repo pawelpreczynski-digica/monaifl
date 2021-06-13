1f2d639be8499e705f10ea257700d0e2a2283531
habibcomsats <habibcomsats@gmail.com>
2021-06-13 22:57:43 +0100

    i1D interpolation added

--- trainer/MNIST/plots.py
+++ trainer/MNIST/plots.py
@@ -3,12 +3,12 @@ import matplotlib.animation as animation
 from matplotlib import style
 import sys
 import os
-#from scipy.interpolate import spline
 from pathlib import Path
 home = str(Path.home())
 import numpy as np
-from scipy.interpolate import  interp1d, Rbf, InterpolatedUnivariateSpline
-
+#from scipy.interpolate import make_interp_spline, BSpline
+#from scipy.interpolate import CubicSpline
+from scipy import interpolate
 logpath = os.path.join(home, "monaifl", "trainer", "save","logs","client")
 logName = 'mnistlog.txt'
 logFile = os.path.join(logpath, logName)
@@ -31,96 +31,34 @@ def animate(i):
     ax1.clear()
     ax2.clear()
 
-    xnew = np.linspace(0, 1, num=40, endpoint=True)
+    xn = np.array(xs)
+    yn = np.array(ys)
+    zn = np.array(zs)
+
+    if (xn.size>1):
+
+        # #define x as 200 equally spaced values between the min and max of original x 
+        xnew = np.linspace(xn.min(), xn.max(), 300) 
+
+        # #define spline
+        spl1 = interpolate.interp1d(xn, yn)
+        spl2 = interpolate.interp1d(xn, zn)
     
-#    iusy = InterpolatedUnivariateSpline(xs, ys)
-#    ynew = iusy(xnew)
+        ynew = spl1(xnew)
+        znew = spl2(xnew)
 
-#    iusz = InterpolatedUnivariateSpline(xs, zs)
-#    znew = iusz(xnew)
+        #create smooth line chart 
+        ax1.plot(xnew, ynew, color='#444444', label='Model Loss')
+        ax2.plot(xnew, znew, color='#2494CC', label='Model Accuracy')
 
-#    rbfy = Rbf(xs, ys)
-#    ynew = rbfy(xnew)
+        ax1.legend()
+        ax1.set_title("Model Training Monitor", fontsize=20)
+        ax1.set_ylabel("Loss", fontsize=16)
 
-#    rbfz = Rbf(xs, zs)
-#    znew = rbfz(xnew)
-
-#    rbfy = interp1d(xs, ys, kind='cubic', bounds_error=False)
-#    ynew = rbfy(xnew)
-
-#    rbfz = interp1d(xs, zs, bounds_error=False)
-#    znew = rbfz(xnew)
-
-#   a_BSpline = interpolate.make_interp_spline(x, y) 
-#    ynew = spline(xs, ys, xnew)
-#    znew = spline(xs, zs, xnew)
-    
-    ax1.plot(xs, ys, color='#444444', label='Model Loss')
-    ax2.plot(xs, zs, color='#2494CC', label='Model Accuracy')
-
-
-#    ax1.plot(xnew, ynew, color='#444444', label='Model Loss')
-#    ax2.plot(xnew, znew, color='#2494CC', label='Model Accuracy')
-
-    ax1.legend()
-    ax1.set_title("Model Training Monitor", fontsize=20)
-    ax1.set_ylabel("Loss", fontsize=16)
-
-    ax2.legend()
-    ax2.set_ylabel("Accuracy(%)", fontsize=16)
-    ax2.set_xlabel("No of Epochs", fontsize=16)
+        ax2.legend()
+        ax2.set_ylabel("Accuracy(%)", fontsize=16)
+        ax2.set_xlabel("No of Epochs", fontsize=16)
 
 ani = animation.FuncAnimation(fig, animate, interval=1000)
-#plt.tight_layout()
+plt.tight_layout()
 plt.show()
-
-# x_new = np.linspace(1, 4, 300)
-
-# a_BSpline = interpolate.make_interp_spline(x, y)
-
-# y_new = a_BSpline(x_new)
-
-
-# plt.plot(x_new, y_new)
-
-#from scipy.interpolate import spline
-
-# 300 represents number of points to make between T.min and T.max
-#xnew = np.linspace(T.min(), T.max(), 300)  
-
-#power_smooth = spline(T, power, xnew)
-
-#plt.plot(xnew,power_smooth)
-#plt.show()
-
-# import numpy as np
-# from scipy.interpolate import Rbf, InterpolatedUnivariateSpline
-# import matplotlib.pyplot as plt
-
-
-# # setup data
-# x = np.linspace(0, 10, 9)
-# y = np.sin(x)
-# xi = np.linspace(0, 10, 101)
-
-# # use fitpack2 method
-# ius = InterpolatedUnivariateSpline(x, y)
-# yi = ius(xi)
-
-# plt.subplot(2, 1, 1)
-# plt.plot(x, y, 'bo')
-# plt.plot(xi, yi, 'g')
-# plt.plot(xi, np.sin(xi), 'r')
-# plt.title('Interpolation using univariate spline')
-
-# # use RBF method
-# rbf = Rbf(x, y)
-# fi = rbf(xi)
-
-# plt.subplot(2, 1, 2)
-# plt.plot(x, y, 'bo')
-# plt.plot(xi, fi, 'g')
-# plt.plot(xi, np.sin(xi), 'r')
-# plt.title('Interpolation using RBF - multiquadrics')
-# plt.show()
-
