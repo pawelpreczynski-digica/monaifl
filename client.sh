bebf715be28adef3ff703c8d5448d9a8119d2f9a
habibcomsats <habibcomsats@gmail.com>
2021-08-04 09:55:24 +0100

    shell sciprts added

---
+++ client.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# client.sh
+cd /home/habib/monaifl/trainer/substra
+pwd
+echo $$
+python testmonai.py
---
+++ server.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# server.sh
+cd /home/habib/monaifl/aggregator/coordinator/src
+pwd
+echo $$
+python server.py
