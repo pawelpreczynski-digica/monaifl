708d5cd5696b4ac3bff8a6d3d851b9db830ab71a
Domenico Siciliani <domenico.siciliani@digica.com>
2021-10-13 10:49:48 +0200

    Moved aggregation within the Client class and started logging improvements

--- hubnspoke/hub/hub.py
+++ hubnspoke/hub/hub.py
@@ -10,25 +10,42 @@ import grpc
 from common.monaifl_pb2_grpc import MonaiFLServiceStub
 from common.monaifl_pb2 import ParamsRequest
 from io import BytesIO
-from common.utils import Mapping
 import torch as t
 import os
 import copy
 import logging
-logging.basicConfig(format='%(asctime)s - %(message)s')
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+from coordinator import FedAvg
+
+class Stage():
+    FEDERATION_INITIALIZATION_STARTED = 'FEDERATION_INITIALIZATION_STARTED'
+    FEDERATION_INITIALIZATION_COMPLETED = 'FEDERATION_INITIALIZATION_COMPLETED'
+    TRAINING_STARTED = 'TRAINING_STARTED'
+    TRAINING_COMPLETED = 'TRAINING_COMPLETED'
+    AGGREGATION_STARTED = 'AGGREGATION_STARTED'
+    AGGREGATION_COMPLETED = 'AGGREGATION_COMPLETED'
+    TESTING_STARTED = 'TESTING_STARTED'
+    TESTING_COMPLETED = 'TESTING_COMPLETED'
+    UPLOAD_STARTED = 'UPLOAD_STARTED'
+    UPLOAD_COMPLETED = 'UPLOAD_COMPLETED'
+    FEDERATION_COMPLETED = 'FEDERATION_COMPLETED'
 
 
 modelpath = os.path.join(cwd, "save","models","hub")
 modelName = "monai-test.pth.tar"
-
 modelFile = os.path.join(modelpath, modelName)
+
 w_loc = list() 
-w_glob = list() 
-request_data = Mapping()
 whitelist = set()
 
+federated_process_logger = logging.getLogger('federated_process') # [%(asctime)s]-[%(model_id)s]-[%(status)s]-[%(trust_name)s]-%(message)s
+federated_process_extra = {'model_id': os.environ.get('MODEL_ID'), 'status':'', 'trust_name':''}
+
+node_status_logger = logging.getLogger('node_status') # [%(asctime)s]-[%(model_id)s]-[%(trust_name)s]-%(message)s
+node_status_extra = {'model_id': os.environ.get('MODEL_ID'), 'trust_name':''}
+
+logging.basicConfig(format='%(asctime)s - %(message)s')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 class Client():
     def __init__(self, address, name):
@@ -44,32 +61,32 @@ class Client():
     
 
     def bootstrap(self):
-        logger.info("bootstrapping with " + self.name)
+        logger.info(f"[{Stage.FEDERATION_INITIALIZATION_STARTED}]-[{self.name}]-bootstrapping...")
         buffer = BytesIO()
         if self.address in whitelist:
-            logger.info(self.name + " is whitelisted")
+            logger.info(f"[{Stage.FEDERATION_INITIALIZATION_STARTED}]-[{self.name}]-fl node is whitelisted")
             if os.path.isfile(modelFile):
-                logger.info(f"buffering the provided initial model {modelFile}...") 
+                logger.info(f"[{Stage.FEDERATION_INITIALIZATION_STARTED}]-[{self.name}]-buffering the provided initial model {modelFile}...") 
                 checkpoint = t.load(modelFile)
                 t.save(checkpoint['weights'], buffer)
             else:
-                logger.info("initial model does not exist, initializing and buffering a new one...")
+                logger.info(f"[{Stage.FEDERATION_INITIALIZATION_STARTED}]-[{self.name}]-initial model does not exist, initializing and buffering a new one...")
                 t.save(self.model.state_dict(), buffer)
             size = buffer.getbuffer().nbytes
             
-            logger.info(f"sending the initial model to {self.name}...")
+            logger.info(f"[{Stage.FEDERATION_INITIALIZATION_STARTED}]-[{self.name}]-sending the initial model...")
             opts = [('grpc.max_receive_message_length', 1000*1024*1024), ('grpc.max_send_message_length', size*2), ('grpc.max_message_length', 1000*1024*1024)]
             self.channel = grpc.insecure_channel(self.address, options = opts)
             client = MonaiFLServiceStub(self.channel)
             fl_request = ParamsRequest(para_request=buffer.getvalue())
             fl_response = client.ModelTransfer(fl_request)
 
-            logger.info(f"received the answer from {self.name}")
+            logger.info(f"[{Stage.FEDERATION_INITIALIZATION_STARTED}]-[{self.name}]-answer received")
             response_bytes = BytesIO(fl_response.para_response)
             response_data = t.load(response_bytes, map_location='cpu')
-            logger.info(f"{self.name} returned status: {response_data}") # Model received OR Error
+            logger.info(f"[{Stage.FEDERATION_INITIALIZATION_COMPLETED}]-[{self.name}]-returned status: {response_data}") # Model received OR Error
         else:
-            logger.error(f"{self.name} is not whitelisted. Please contact admin for permissions")
+            logger.error(f"[{Stage.FEDERATION_INITIALIZATION_COMPLETED}]-[{self.name}]-fl node is not whitelisted. Please contact admin for permissions")
 
     def train(self, epochs):
         self.data = {"epochs": epochs}
@@ -77,18 +94,17 @@ class Client():
         t.save(self.data, buffer)
         size = buffer.getbuffer().nbytes
 
-        logger.info(f"sending the training request to {self.name} for {epochs} epochs...")
+        logger.info(f"[{Stage.TRAINING_STARTED}]-[{self.name}]-sending the training request for {epochs} epochs...")
         opts = [('grpc.max_receive_message_length', 1000*1024*1024), ('grpc.max_send_message_length', size*2), ('grpc.max_message_length', 1000*1024*1024)]
         self.channel = grpc.insecure_channel(self.address, options = opts)
         client = MonaiFLServiceStub(self.channel)
         fl_request = ParamsRequest(para_request=buffer.getvalue())
         fl_response = client.MessageTransfer(fl_request)
 
-        logger.info(f"received the training started ack from {self.name}")
+        logger.info(f"[{Stage.TRAINING_STARTED}]-[{self.name}]-answer received")
         response_bytes = BytesIO(fl_response.para_response)
         response_data = t.load(response_bytes, map_location='cpu')
-        print(response_data)
-        logger.info(f"{self.name} returned status: {response_data}") # Training started 
+        logger.info(f"[{Stage.TRAINING_COMPLETED}]-[{self.name}]-returned status: {response_data}") # Training started 
         return response_data
     
     def status(self):
@@ -97,7 +113,7 @@ class Client():
         t.save(self.data, buffer)
         size = buffer.getbuffer().nbytes
 
-        logger.info(f"checking node status: {self.name}...")
+        logger.info(f"[{self.name}]-checking fl node status...")
         opts = [('grpc.max_receive_message_length', 1000*1024*1024), ('grpc.max_send_message_length', size*2), ('grpc.max_message_length', 1000*1024*1024)]
         self.channel = grpc.insecure_channel(self.address, options = opts)
         client = MonaiFLServiceStub(self.channel)
@@ -106,7 +122,7 @@ class Client():
 
         response_bytes = BytesIO(fl_response.para_response)
         response_data = t.load(response_bytes, map_location='cpu')
-        logger.info(f"{self.name} returned status: {response_data}") # Training completed OR Training in progress 
+        logger.info(f"[{self.name}]-returned status: {response_data}")
         return response_data
   
     def gather(self):
@@ -115,7 +131,7 @@ class Client():
         t.save(self.data, buffer)
         size = buffer.getbuffer().nbytes
 
-        logger.info(f"Sending the trained model request to {self.name}...")
+        logger.info(f"[{Stage.AGGREGATION_STARTED}]-[{self.name}]-Sending the trained model request...")
         opts = [('grpc.max_receive_message_length', 1000*1024*1024), ('grpc.max_send_message_length', size*2), ('grpc.max_message_length', 1000*1024*1024)]
         self.channel = grpc.insecure_channel(self.address, options = opts)
         client = MonaiFLServiceStub(self.channel)
@@ -126,6 +142,30 @@ class Client():
         response_bytes = BytesIO(fl_response.para_response)    
         response_data = t.load(response_bytes, map_location='cpu')
         return response_data
+
+    def aggregate(self):
+        logger.info(f"Aggregating with Node: {self.name}...") 
+        checkpoint = self.gather()
+        for k in checkpoint.keys():
+            if k == "epoch":
+                #epochs = checkpoint['epoch']
+                logger.info(f"Best Epoch at Client: {checkpoint['epoch']}...") 
+            elif k == "weights":
+                w = checkpoint['weights']
+                logger.info(f"Copying weights from {self.name}...")
+                w_loc.append(copy.deepcopy(w))
+                logger.info(f"Aggregating weights from {self.name}...")
+                w_glob = FedAvg(w_loc)
+            elif k == "metric":
+                logger.info(f"Best Metric at Client: {checkpoint['metric']}..." )
+            else:
+                logger.info(f"Server does not recognized the data sent from {self.name}")
+        cpt = {#'epoch': 1, # to be determined
+            'weights': w_glob#,
+            #'metric': 0 # to be aggregated
+            }
+        t.save(cpt, modelFile)
+        logger.info(f"aggregation with {self.name} completed")
         
     def test(self):
         buffer = BytesIO()
--- hubnspoke/hub/start.py
+++ hubnspoke/hub/start.py
@@ -4,22 +4,13 @@ cwd = str(Path.cwd())
 import sys
 sys.path.append('.')
 import os
-from hub import Client
-import time
+from hub import Client, Stage
 import logging
 import concurrent.futures
-import copy
-import time
-import torch as t
-from coordinator import FedAvg
 import shutil
 from datetime import datetime
 import json
 import boto3
-
-logging.basicConfig(format='%(asctime)s - %(message)s')
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
 FL_CLIENT_ENDPOINTS = json.loads(os.environ.get('FL_CLIENT_ENDPOINTS'))
 ENVIRONMENT = os.environ.get('ENVIRONMENT')
@@ -27,10 +18,34 @@ MODEL_ID = os.environ.get('MODEL_ID')
 
 modelpath = os.path.join(cwd, "save","models","hub")
 modelName = "monai-test.pth.tar"
-
 modelFile = os.path.join(modelpath, modelName)
-w_loc = []
-clients = [Client(fl_client['flclientendpoint'], fl_client['name']) for fl_client in FL_CLIENT_ENDPOINTS]
+
+federated_process_logger = logging.getLogger('federated_process')
+syslog = logging.StreamHandler()
+formatter = logging.Formatter('[%(asctime)s]-[%(model_id)s]-[%(status)s]-[%(trust_name)s]-%(message)s')
+syslog.setFormatter(formatter)
+federated_process_logger.setLevel(logging.INFO)
+federated_process_logger.addHandler(syslog)
+
+node_status_logger = logging.getLogger('node_status')
+syslog = logging.StreamHandler()
+formatter = logging.Formatter('[%(asctime)s]-[%(model_id)s]-[%(trust_name)s]-%(message)s')
+syslog.setFormatter(formatter)
+node_status_logger.setLevel(logging.INFO)
+node_status_logger.addHandler(syslog)
+
+main_logger = logging.getLogger('main')
+syslog = logging.StreamHandler()
+formatter = logging.Formatter('[%(asctime)s]-[%(model_id)s]-[%(status)s]-%(message)s')
+syslog.setFormatter(formatter)
+main_logger.setLevel(logging.INFO)
+main_logger.addHandler(syslog)
+main_logger = logging.LoggerAdapter(main_logger, extra={'model_id': MODEL_ID})
+
+logging.basicConfig(format='%(asctime)s - %(message)s')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 def model_spread_plan(client):
     try:
@@ -38,7 +53,7 @@ def model_spread_plan(client):
             # spreading model to nodes
             client.bootstrap()
     except:
-        logger.info(f"client {client.address} is dead...")
+        logger.info(f"client {client.name} is dead...")
 
 def train_plan(client):
     try:
@@ -46,35 +61,16 @@ def train_plan(client):
             # initializing training on nodes
             client.train(epochs='1')
     except:
-        logger.info(f"client {client.address} is dead...")
+        logger.info(f"client {client.name} is dead...")
     
 def aggregate(client):
     try:
         if(client.status() == "alive"):
-            logger.info(f"Aggregating with Node: {client.address}...") 
-            checkpoint = client.gather()
-            for k in checkpoint.keys():
-                if k == "epoch":
-                    #epochs = checkpoint['epoch']
-                    logger.info(f"Best Epoch at Client: {checkpoint['epoch']}...") 
-                elif k == "weights":
-                    w = checkpoint['weights']
-                    logger.info(f"Copying weights from {client.address}...")
-                    w_loc.append(copy.deepcopy(w))
-                    logger.info(f"Aggregating weights from {client.address}...")
-                    w_glob = FedAvg(w_loc)
-                elif k == "metric":
-                    logger.info(f"Best Metric at Client: {checkpoint['metric']}..." )
-                else:
-                    logger.info(f"Server does not recognized the data sent from {client.address}")
-            cpt = {#'epoch': 1, # to be determined
-                'weights': w_glob#,
-                #'metric': 0 # to be aggregated
-                }
-            t.save(cpt, modelFile)
-            logger.info(f"aggregation with {client.address} completed")
-    except:
-        logger.info(f"client {client.address} is dead...")
+            # aggregating models from nodes
+            client.aggregate()
+    except Exception as ex:
+        print(ex)
+        logger.info(f"client {client.name} is dead...")
 
 def test_plan(client):
     try:
@@ -82,7 +78,7 @@ def test_plan(client):
             # testing models on nodes
             client.test()
     except:
-        logger.info(f"client {client.address} is dead...")
+        logger.info(f"client {client.name} is dead...")
     
 def stop_now(client):
     try:
@@ -90,7 +86,7 @@ def stop_now(client):
             # asking nodes to stop
             client.stop()
     except:
-        logger.info(f"client {client.address} is dead...")
+        logger.info(f"client {client.name} is dead...")
 
 def upload_results_in_s3_bucket(source_path: str, bucket_name: str = 'flip-uploaded-federated-data-bucket'):
     bucket_name += '-' + ENVIRONMENT
@@ -112,7 +108,8 @@ def upload_results_in_s3_bucket(source_path: str, bucket_name: str = 'flip-uploa
 
 
 if __name__ == '__main__':
-    logger.info("Central Hub initialized")
+    logger.info("hub started")
+    clients = [Client(fl_client['flclientendpoint'], fl_client['name']) for fl_client in FL_CLIENT_ENDPOINTS]
 
     global_round = 1
 
@@ -131,13 +128,12 @@ if __name__ == '__main__':
             result = executor.map(test_plan, clients)    
         
         logger.info(f"Global round {round+1}/{global_round} completed")
-        print("-------------------------------------------------")
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
         result = executor.map(stop_now, clients)  
     
     # all processes are excuted 
-    logger.info(f"Model Training is completed across all sites and current global model is available at following location...{modelFile}")
+    logger.info(f"Model Training is completed across all sites and current global model is available at {modelFile}")
 
     upload_results_in_s3_bucket(modelpath)
 
